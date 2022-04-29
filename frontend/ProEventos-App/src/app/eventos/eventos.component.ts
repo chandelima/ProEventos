d6e3635c61fd522169f4b2434f78d9268e76e911
chandelima <49618629+chandelima@users.noreply.github.com>
2022-04-29 10:49:51 -0300

    Angular Events list page

--- frontend/ProEventos-App/src/app/eventos/eventos.component.ts
+++ frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public filteredEvents: any = [];
+  public imageShow: boolean = true;
+  public imageWidth: number = 150;
+  private _listFilter: string = '';
+
+  public get listFilter(): string {
+    return this._listFilter;
+  }
+
+  public set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredEvents = this.listFilter ? this.eventFilter(this.listFilter) : this.eventos;
+  }
+
+  eventFilter(filter: string): any {
+    filter = filter.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filter) !== -1 ||
+                       evento.local.toLocaleLowerCase().indexOf(filter) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,10 +37,17 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  public imageState(): void {
+    this.imageShow = !this.imageShow;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos')
         .subscribe({
-          next: response => this.eventos = response,
+          next: response => {
+            this.eventos = response;
+            this.filteredEvents = response;
+          },
           error: error => console.log(error)
         });
   }
